54c7edf1c0a5eb21766edfde0e23de008fba1a02
FranciscoBraaz <francisco_braaz@hotmail.com>
2021-07-24 18:44:43 -0300

    Criação dos da página home e header

---
+++ src/components/SignInButton/index.js
@@ -0,0 +1,20 @@
+import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+export function SignInButton() {
+  const isUserLogin = true;
+
+  return isUserLogin ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Francisco
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
+}
